b06bff2a9e1ac9619b6b236a25574c4764742428
valentino <v.arballo@itecriocuarto.org.ar>
2024-08-06 18:00:55 -0300

    listo

---
+++ main.js
@@ -0,0 +1,16 @@
+const calculator = (distance, frequency) => {
+  return 8.656 * Math.sqrt(distance / frequency);
+}
+
+const distance = document.getElementById("distanceInput");
+const frequency = document.getElementById("frequencyInput");
+const result = document.getElementById("result");
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  let fresnel = calculator(distance.value, frequency.value);
+  if (isNaN(fresnel)) {
+      fresnel = 0;
+  }
+  result.textContent = "El radio de la zona de Fresnel es de " + fresnel.toFixed(2) + " metros.";
+}
